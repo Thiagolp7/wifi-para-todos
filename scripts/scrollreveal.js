3f3b60e52663ab456b8fb651e93ab803dae26f5b
ThiagoLp <thiagoluiz.ps@hotmail.com>
2021-12-09 15:30:28 -0300

    scrollreveal added

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.reveal-100', { delay: 100 })
+ScrollReveal().reveal('.reveal-200', { delay: 200 })
+ScrollReveal().reveal('.reveal-300', { delay: 300 })
+ScrollReveal().reveal('.reveal-600', { delay: 600 })
