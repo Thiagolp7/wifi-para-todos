a486eab36f5c5b3f77295512678f589d839a23f8
ThiagoLp <thiagoluiz.ps@hotmail.com>
2021-12-13 13:14:14 -0300

    change js file and localstorage theme added

--- scripts/main.js
+++ scripts/main.js
@@ -1,149 +1,159 @@
-// Change menu bar background when close at header intro
-const headerIntro = document.querySelector('#intro')
-const menuBar = document.querySelector('#menu')
-const logoTexts = document.querySelectorAll('.logo__text')
-const menuLinks = document.querySelectorAll('.menu__link')
-const headerIntroPosition = headerIntro.offsetTop
-let menuBarBgColorOn = false
+// Elements
+const htmlElement = document.querySelector('html');
+const menuSection = document.querySelector('#menu');
+const headerIntro = document.querySelector('#intro');
+const logoTexts = document.querySelectorAll('.logo__text');
+const btnSwitchTheme = document.querySelector('#btn-switch-theme');
+const btnSwitchThemeImg = document.querySelectorAll('.btn-switch-theme__img');
+const btnSwitchThemeImgLight = document.querySelector('#light-theme-img');
+const btnSwitchThemeImgDark = document.querySelector('#dark-theme-img');
+const menuLinksContainer = document.querySelector('#menu__links-container');
+const menuLinks = document.querySelectorAll('.menu__link');
+const btnMenuMobile = document.querySelector('#btn-menu-mobile');
+const btnMenuLines = document.querySelectorAll('.menu-mobile__line');
+const formTabs = document.querySelectorAll('.tab');
+const formContents = document.querySelectorAll('.form__content');
 
-// menu mobile
-const btnMenuMobile = document.querySelector('#btn-menu-mobile')
-const btnMenuLines = document.querySelectorAll('.menu-mobile__line')
-const menuNav = document.querySelector('#menu__links-container')
-const btnSwitchThemeImg = document.querySelectorAll('.btn-switch-theme__img')
+// Controllers
+const menuSectionBottom = menuSection.offsetTop + menuSection.offsetHeight;
+const headerIntroTop = headerIntro.offsetTop;
+let menuSectionHasBackground = false;
 
-function changeMenuBarBgcolor() {
-  const pagePosition = window.scrollY
-  const menuBarBgOn = pagePosition > headerIntroPosition
+// Functions
+function AddClassInElements(elements, className) {
+  elements.forEach(element => {
+    element.classList.add(className);
+  })
+}
 
-  if (menuBarBgOn) {
-    menuBar.classList.add('bg-active')
+function RemoveClassInElements(elements, className) {
+  elements.forEach(element => {
+    element.classList.remove(className);
+  })
+}
 
-    logoTexts.forEach(text => {
-      text.classList.add('bg-active')
-    })
+function MenuSectionBackgroundOn() {
+  const active = 'bg-active'
+  menuSection.classList.add(active);
+  AddClassInElements(logoTexts, active);
+  AddClassInElements(btnSwitchThemeImg, active);
+  AddClassInElements(menuLinks, active);
+  AddClassInElements(btnMenuLines, active);
+  menuSectionHasBackground = true;
+}
 
-    btnSwitchThemeImg.forEach(img =>{
-      img.classList.add('bg-active')
-    })
+function MenuSectionBackgroundOff() {
+  const active = 'bg-active';
+  menuSection.classList.remove(active);
+  RemoveClassInElements(logoTexts, active);
+  RemoveClassInElements(btnSwitchThemeImg, active);
+  RemoveClassInElements(menuLinks, active);
+  RemoveClassInElements(btnMenuLines, active);
+  menuSectionHasBackground = false;
+}
 
-    menuLinks.forEach(link => {
-      link.classList.add('bg-active')
-    })
-
-    btnMenuLines.forEach(line => {
-      line.classList.add('bg-active')
-    })
-
-    menuBarBgColorOn = true
+function CheckIfMenuCrossedIntro() {
+  const pagePosition = window.scrollY;
+  const crossed = pagePosition + menuSectionBottom > headerIntroTop;
+  if (crossed) {
+    return true
   } else {
-    menuBar.classList.remove('bg-active')
-
-    logoTexts.forEach(text => {
-      text.classList.remove('bg-active')
-    })
-
-    btnSwitchThemeImg.forEach(img =>{
-      img.classList.remove('bg-active')
-    })
-
-    menuLinks.forEach(link => {
-      link.classList.remove('bg-active')
-    })
-
-    btnMenuLines.forEach(line => {
-      line.classList.remove('bg-active')
-    })
-
-    menuBarBgColorOn = false
+    return false
   }
 }
 
-function menuMobileOnOff() {
-  btnMenuLines.forEach(lines => {
-    lines.classList.toggle('active')
-  })
-  menuNav.classList.toggle('active')
-
-  if (!menuBarBgColorOn) {
-    menuBar.classList.toggle('bg-active')
-
-    logoTexts.forEach(text => {
-      text.classList.toggle('bg-active')
-    })
-
-    btnMenuLines.forEach(line => {
-      line.classList.toggle('bg-active')
-    })
+function MenuSectionSetBackground() {
+  if (CheckIfMenuCrossedIntro()) {
+    MenuSectionBackgroundOn();
+  } else {
+    MenuSectionBackgroundOff();
   }
 }
 
-window.addEventListener('load', () => {
-  document.addEventListener('scroll', changeMenuBarBgcolor)
-  btnMenuMobile.addEventListener('click', menuMobileOnOff)
-})
-
-
-// form-container
-
-const formTabs = document.querySelectorAll('.tab')
-const formContents = document.querySelectorAll('.form__content')
-
-formTabs.forEach(tab => {
-  tab.addEventListener('click', event => {
-    formTabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-
-    tab.classList.add('active')
-
-    formContents.forEach(content =>{
-      content.classList.remove('active')
-
-      const contentId = content.id.replace('form-', '')
-      const tabID = tab.id.replace('tab-', '')
-      if(contentId === tabID ){
-        content.classList.add('active')
-      }
-    })
-  })
-})
-
-
-// close menu-mobile on move scroll
-function MenuMobileClose(){
-  btnMenuLines.forEach(lines => {
-    lines.classList.remove('active')
-  })
-  menuNav.classList.remove('active')
+function MenuMobileOpenClose() {
+  menuLinksContainer.classList.toggle('active');
+  if (!menuSectionHasBackground && !CheckIfMenuCrossedIntro()) {
+    MenuSectionBackgroundOn();
+  } else if (menuSectionHasBackground && !CheckIfMenuCrossedIntro()) {
+    MenuSectionBackgroundOff();
+  }
 }
 
-window.addEventListener('scroll', MenuMobileClose)
+function MenuMobileClose() {
+  menuLinksContainer.classList.remove('active');
+}
 
-
-// switch theme button
-const htmlElement = document.querySelector('html')
-const switchBtn = document.querySelector('#switch-theme')
-
-switchBtn.addEventListener('click', click =>{
-  switchBtn.classList.toggle('active')
-  
-  if(switchBtn.classList.contains('active')){
-    htmlElement.setAttribute('data-theme', 'dark')
-    btnSwitchThemeImg.forEach(img => {
-      img.classList.remove('active')
-      if(img.id == 'dark-theme'){
-        img.classList.add('active')
-      }
-    })
-  } else {
-    htmlElement.removeAttribute('data-theme')
-    btnSwitchThemeImg.forEach(img => {
-      img.classList.remove('active')
+function FormTabChange(){
+  formTabs.forEach(tab => {
+    tab.addEventListener('click', event => {
+      const active = 'active'
       
-      if(img.id == 'light-theme'){
-        img.classList.add('active')
-      }
+      RemoveClassInElements(formTabs, active);
+      
+      tab.classList.add(active)
+      
+      formContents.forEach(content =>{
+        content.classList.remove(active);
+        
+        const contentId = content.id.replace('form-', '');
+        const tabId = tab.id.replace('tab-', '');
+  
+        if(contentId === tabId ){
+          content.classList.add(active);
+        }
+      })
     })
+  })
+}
+
+function CheckDataTheme(){
+  const currentTheme = htmlElement.getAttribute('data-theme');
+  return currentTheme
+} 
+
+function SaveDataTheme(theme){
+  localStorage.setItem('theme', theme);
+}
+
+function LoadDataTheme(){
+  const theme = localStorage.theme;
+  if(theme){
+    SetTheme(theme)
   }
-})
+}
+
+function SetTheme(theme){
+  const setTheme = theme;
+  const active = 'active';
+
+  if (setTheme == 'dark'){
+    RemoveClassInElements(btnSwitchThemeImg, active);
+    btnSwitchThemeImgDark.classList.add(active);
+    htmlElement.setAttribute('data-theme', 'dark');
+    SaveDataTheme('dark');
+  } else if(setTheme == 'light'){
+    RemoveClassInElements(btnSwitchThemeImg, active);
+    btnSwitchThemeImgLight.classList.add(active);
+    htmlElement.setAttribute('data-theme', 'light');
+    SaveDataTheme('light');
+  }
+}
+
+function ChangeTheme(){
+  if(CheckDataTheme() == 'light'){
+    SetTheme('dark');
+  } else if (CheckDataTheme() == 'dark'){
+    SetTheme('light');
+  }
+}
+
+// Executations
+window.addEventListener('load', LoadDataTheme);
+window.addEventListener('load', CheckIfMenuCrossedIntro);
+window.addEventListener('load', MenuSectionSetBackground);
+window.addEventListener('load', FormTabChange);
+window.addEventListener('scroll', CheckIfMenuCrossedIntro);
+window.addEventListener('scroll', MenuSectionSetBackground);
+btnMenuMobile.addEventListener('click', MenuMobileOpenClose);
+window.addEventListener('scroll', MenuMobileClose);
+btnSwitchTheme.addEventListener('click', ChangeTheme);
